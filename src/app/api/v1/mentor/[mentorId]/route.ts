26fd90fe82a3598adb2cc135f6e74ef65707ef3f
Nozomi Fukawa <noppi5959@gmail.com>
2024-03-29 00:04:30 +0900

    add: mentor-api

---
+++ src/app/api/v1/mentor/[mentorId]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma, prismaConnect } from "@/utils/prisma/prisma";
+
+export const GET = async(req: NextRequest, {params}: {params: {mentorId: Number}}) => {
+    try {
+        await prismaConnect();
+        const mentorId = params.mentorId;
+        const messages = await prisma.message.findMany({
+            where: {
+                mentorId: Number(mentorId)
+            }
+        })
+        return NextResponse.json({messages}, {status: 201})
+    } catch(error) {
+        return NextResponse.json({error}, {status: 500})
+    } finally {
+        prisma.$disconnect();
+    }
+}
