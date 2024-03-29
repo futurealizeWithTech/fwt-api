4c3dc6767e06104604b3c27228b659779f04e235
HASURO <rentaro170928@icloud.com>
2024-03-29 19:53:55 +0900

    hotfix: cores #3

--- src/app/api/v1/mentor/[mentorId]/route.ts
+++ src/app/api/v1/mentor/[mentorId]/route.ts
@@ -13,7 +13,7 @@ export const GET = async (
         mentorId: Number(mentorId),
       },
     });
-    return NextResponse.json({ messages }, { status: 201 });
+    return NextResponse.json({ messages }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   } finally {
---
+++ src/utils/coreHeaders.ts
@@ -0,0 +1,5 @@
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', // OPTONSを追加
+  'Access-Control-Allow-Headers': 'Content-Type', // 追加
+}
